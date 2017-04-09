52758d06da2ac12cd27a7eab420bb70c279b7fcf
ormalkai <ormalkai@walla.co.il>
2017-04-09 22:02:56 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 
 #include "Game.h"
 #include "Debug.h"
+#include <iostream>
 
-int main() 
+int main(int argc, char** argv) 
 {
 	// init log
 	Debug::instance()->init("game.log", true, true, Debug::DBG_ERROR);
 	DBG(Debug::DBG_ERROR, "hello world");
+	int number;
+	std::cin >> number;
 
 	//Game& game = Game::getInstance();
 	// add is initialize ?
