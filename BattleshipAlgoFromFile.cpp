76e70316e89cca42376bc3c8d6de050cd712fb99
ormalkai <ormalkai@walla.co.il>
2017-04-11 22:46:24 +0300

    Hila's code

--- BattleshipAlgoFromFile.cpp
+++ BattleshipAlgoFromFile.cpp
@@ -1,4 +1,30 @@
 #include "BattleshipAlgoFromFile.h"
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+		std::not1(std::ptr_fun<int, int>(std::isspace))));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(),
+		std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+	ltrim(s);
+	rtrim(s);
+}
 
 BattleshipAlgoFromFile::BattleshipAlgoFromFile(int id)
 {
@@ -11,9 +37,81 @@ void BattleshipAlgoFromFile::setBoard(const char** board, int numRows, int numCo
 
 std::pair<int, int> BattleshipAlgoFromFile::attack()
 {
-	return *(new pair<int, int>);
+	// TODO what if empty
+	std::pair<int, int> attack = m_attackQueue[m_attackQueue.size() - 1];
+	m_attackQueue.pop_back();
+	return attack;
 }
 
 void BattleshipAlgoFromFile::notifyOnAttackResult(int player, int row, int col, AttackResult result)
 {
 }
+
+//splits string s to a vector
+static std::vector<std::string> split(const std::string &s, char delim)
+{
+	std::vector<std::string> elems;
+	std::stringstream strSt(s);
+	std::string item;
+
+	while (std::getline(strSt, item, delim)) //we could ommit the 'std::' notion from here.
+	{
+		elems.push_back(item); //push_back is a function of vector class, it inserted to the back of the vector.
+	}
+
+	return elems;
+}
+
+void BattleshipAlgoFromFile::AttackFileParser(string& attackPath) {
+	vector<pair<int, int>> attackMoves;
+	vector<string> tokens;
+	int i, j;
+	ifstream fin(attackPath);
+	string line;
+	while (Utils::instance().safeGetline(fin, line))
+	{
+		tokens = split(line, ',');
+		if (tokens.size() != 2)
+		{
+			return;
+		}
+		trim(tokens[0]);
+		i = stoi(tokens[0]);
+		trim(tokens[1]);
+		j = stoi(tokens[1]);
+		pair<int, int> aPair(i, j);
+		m_attackQueue.push_back(aPair);
+	}
+
+
+}
+
+//int BattleshipAlgoFromFile::checkLine(string str, int k) {
+//
+//	int i;
+//	if (k == 1) {
+//		i = stoi(str);
+//	}
+//
+//	if (k == 2) {
+//		i = cleanSpace();
+//		i = stoi(str[i]);
+//	}
+//	if (i >= 1 && i <= 10) {
+//		if (cleanSpace() == str.length())
+//			return i;
+//	}
+//	return -1;
+//
+//}
+//
+//int BattleshipAlgoFromFile::cleanSpace(string str, int index) {
+//
+//	while (SPACE == index<str.length() && str[index])
+//	{
+//		index++;
+//	}
+//	return index;
+//}
+
+
--- BattleshipAlgoFromFile.h
+++ BattleshipAlgoFromFile.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "PlayerAlgo.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include <queue>
 
 using namespace std;
@@ -10,7 +14,12 @@ private:
 	char** m_board;
 	int m_rows;
 	int m_cols;
-	std::queue<pair<int,int>> m_attackQueue;
+	vector<pair<int, int>> m_attackQueue;
+
+	void AttackFileParser(string & attackPath);
+	int checkLine(string str, int k);
+	int cleanSpace(string str, int index);
+
 
 
 public:
@@ -18,25 +27,26 @@ public:
 	~BattleshipAlgoFromFile() = default;
 
 	/*
-		Initialize attack queue
+	Initialize attack queue
 	*/
 	int parseAttackFile();
 
 	/*
-		Initialize data members
+	Initialize data members
 	*/
 	void setBoard(const char** board, int numRows, int numCols);
-	
+
 	/*
-		Simulate attack- pop attack from attack queue
+	Simulate attack- pop attack from attack queue
 	*/
 	std::pair<int, int> attack();
 
 	/*
-		Empty implementation
+	Empty implementation
 	*/
 	void notifyOnAttackResult(int player, int row, int col, AttackResult result);
-	
+
+
 
 
 
