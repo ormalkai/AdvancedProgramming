299b62bcc0cd54fbc2750a39784d08980e1ab502
ormalkai <ormalkai@walla.co.il>
2017-04-25 21:26:56 +0300

    Adding description of the algorithm

--- BattleshipAlgoSmart.h
+++ BattleshipAlgoSmart.h
@@ -3,10 +3,116 @@
 #include "PlayerAlgo.h"
 #include <queue>
 #include "Board.h"
-#include <set>
 #include <list>
 #include "PriorityQueue.h"
 
+/**
+ *
+ * Description of the algorithm:
+ * Basically the player has two modes of attacks, the first is Hunt, the second is Target.
+ * In Hunt mode, the player wants to find a cell which contains a ship or part of a ship, then
+ * switch to target mode. In target mode the player is pointed the attacks until the ship is sunk,
+ * and so on.
+ * 
+ * Hunt mode:
+ *		In Hunt mode the player creates histogram of how many ships can use the cell for each cell.
+ *		For example, at the corners, the value can be at most 7:
+ *			1. One foe size of size 1
+ *			2. Two for ships of size 2 (one vertical and one horizontal)
+ *			3. Two for ships of size 3 (one vertical and one horizontal)
+ *			4. Two for ships of size 4 (one vertical and one horizontal)
+ *		The same calculation can be executed for cells in the middle of the board.
+ *		During the calculation, the player taking care of game limitations:
+ *			1. There are no Adjacent ships.
+ *			2. Ship can not be outside the border of the board.
+ *		The histogram is updated every attack (will be explained later).
+ *		The player holds a priority queue which maps between the cell to its value in the histogram.
+ *		That way the player in hunt mode will attack the cells with the highest probabillity it will contain
+ *		a ship.
+ *		
+ *	Target mode:
+ *		In Target mode the player holds a queue of potential cells which can contain the part of the targeted ship.
+ *		In the first hit, all 4 neighbors(if exists) are inserted to the queue, in the second hit we can determine
+ *		the direction of the ship, horizontal or vertical, therefore we can make corrections in the queue and remove, 
+ *		the potential cells from the other axis. from that point we will add neighbors to the queue in every hit only
+ *		if the are in the same direction of the ship.
+ *		The player will exit target mode only after the ship is sunk.
+ *		While exiting from target mode in returning to hunt mode the player updates the histogram according to the ship
+ *		sunk.
+ *		
+ *	Creating and Updating the Histogram:
+ *		TODO GAL add your staff here
+ *		
+ *	Handling attack results:
+ *		We need to define between several situations:
+ *		1. The player made an attack and it was successfull and the player is in hunt mode:
+ *				That means that the player found a part of the ship, we need to add its
+ *				neighbors to the target queue and switch to target mode.
+ *		2. The player made an attack and it was successfull and the player is in target mode:
+ *				If this is the first hit in target mode we can determine the direction
+ *				of the ship vertical or horizontal, and remove irrelevant cells from the 
+ *				attacks queue, if it is not the first hit in target mode we need to add only one
+ *				cell to attack queue, the one that is a neighbor to the attacked cell in the 
+ *				same direction of the ship(if it exists).
+ *		
+ *		3. The player made an attack and missed and the player is in hunt mode:
+ *				The player need to update the histogram  in the area of the missed cell
+ *				and stay in hunt mode.
+ *		4. The player made an attack and missed and the player is in target mode:
+ *				Bassically, nothing to do, stay in target mode, next time the player will
+ *				take another cell from the queue.
+ *		
+ *		5. The player made an attack and ship sunk and the player is in hunt mode:
+ *				This case can happen only if the player sunk a ship of size 1.
+ *				Therefore the handling is the same as sunk in target mode. the player
+ *				needs to update the histogram accroding to the new ship, and stay in hunt mode.
+ *		6. The player made an attack and ship sunk and the player is in target mode:
+ *				In this case the player needs to update the histogram according to the new ship
+ *				and switch to hunt mode.
+ *		
+ *		7. The other player made an attack and it was successfull and the player is in hunt mode:
+ *				Nothing to do
+ *		8. The other player made an attack and it was successfull and the player is in target mode:
+ *				Nothing to do
+ *				
+ *		9. The other player made an attack and it was SELF successfull and the player is in hunt mode:
+ *				The player should act exactly like it was him discovering the ship, he needs to switch
+ *				to Target mode and add the neighbors of the attacked cell to the target queue.
+ *		10. The other player made an attack and it was SELF successfull and the player is in target mode:
+ *				The player can not exit from target mode unless the ship is sunk, therefore that attacked
+ *				value of the attacked cell will be updated to MAX_INT in the histogram, that way it will
+ *				be the first cell the player will choose in the next attack in hunt mode, the player 
+ *				should recognize the mark of MAX_INT and switch to target mode again with its neighbors
+ *				and attack one of them.
+ *				
+ *		11. The other player made an attack and missed and the player is in hunt mode:
+ *				In this case that means that there is no ship in the attacked cell, therfore
+ *				the histogram should be updated, the attacked cell value should be 0, and its
+ *				area should be updated accordingly.
+ *		12. The other player made an attack and missed and the player is in target mode:
+ *				In this case that means that there is no ship in the attacked cell, therfore
+ *				the histogram should be updated, the attacked cell value should be 0, and its
+ *				area should be updated accordingly.
+ *		
+ *		13. The other player made an attack and ship sunk and the player is in hunt mode:
+ *				nothing to do
+ *		14. The other player made an attack and ship sunk and the player is in hunt mode:
+ *				nothing to do
+ *		
+ *		15. The other player made an attack and SELF ship sunk and the player is in hunt mode:
+ *				This case can happen only if the other player sunk a ship of size 1.
+ *				Therefore the handling is the same as sunk in target mode. the player
+ *				needs to update the histogram accroding to the new ship, and stay in hunt mode.
+ *		16. The other player made an attack and SELF ship sunk and the player is in target mode:
+ *				This case can happen only if the other player sunk its own ship while the player
+ *				is in target mode, the trivial example is sinking ship of size 1, but non-trivial
+ *				example will be hitting ship of size 2 twice in a row, while the target mode
+ *				is pointing to ship of size 4. In this case the player should find out the size of 
+ *				the sunk ship by looking that MAX_INT marks next to the sunk cell, this sequence 
+ *				should be the ship, then the histogram should be updated as it should be updated 
+ *				while sinking in target mode.
+ *
+ */
 
 class BattleshipAlgoSmart : public PlayerAlgo
 {
